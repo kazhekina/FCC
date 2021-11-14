8e3bd6f257abe0d5e3dbe9210427497be17a7eff
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-15 01:43:20 +0300

    task 2

--- debbuging.js
+++ debbuging.js
@@ -5,3 +5,11 @@
 // console.log(a);
 // let sumAB = a + b;
 // console.log(sumAB);
+
+
+// let seven = 7;
+// let three = "3";
+// console.log(seven + three);
+// // Add your code below this line
+// console.log(typeof seven);
+// console.log(typeof three);
