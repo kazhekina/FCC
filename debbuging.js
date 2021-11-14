72e365ccefc425ba43a62fa54d7d8a83048185b1
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-15 01:59:36 +0300

    task 8

--- debbuging.js
+++ debbuging.js
@@ -48,3 +48,12 @@
 //   }
 //   let result = getNine();
 //   console.log(result);
+
+
+// function raiseToPower(b, e) {
+//     return Math.pow(b, e);
+//   }
+//   let base = 2;
+//   let exp = 3;
+//   let power = raiseToPower(base, exp);
+//   console.log(power);
