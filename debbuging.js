da366110ed9aa41a70137311169f4d62cc955ca6
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-15 01:45:34 +0300

    task 3

--- debbuging.js
+++ debbuging.js
@@ -13,3 +13,9 @@
 // // Add your code below this line
 // console.log(typeof seven);
 // console.log(typeof three);
+
+
+// let receivables = 10;
+// let payables = 8;
+// let netWorkingCapital = receivables - payables;
+// console.log(`Net working capital is: ${netWorkingCapital}`);
