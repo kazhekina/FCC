1fdf1bfc24721e3660f2e1c51f8b8d64fafac6fe
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-16 01:02:41 +0300

    task 2

--- basic_data_structures.js
+++ basic_data_structures.js
@@ -2,5 +2,11 @@
 
 // let yourArray = ['thirty', 2007, true, 26, 'love']; // change this line
 
+// 2
 
+// let myArray = ["a", "b", "c", "d"];
+// // change code below this line
+// myArray[1] = "the new and improved b";
+// // change code above this line
+// console.log(myArray);
 
