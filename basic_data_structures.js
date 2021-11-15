f225e2554324ad6e15b25f98d06556298d5100c9
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-16 01:09:54 +0300

    task 9

--- basic_data_structures.js
+++ basic_data_structures.js
@@ -78,3 +78,13 @@
   
 //   console.log(copyMachine([true, false, true], 2));
 
+//   9
+
+//   function spreadOut() {
+//     let fragment = ['to', 'code'];
+//     let sentence = ['learning', ...fragment, 'is', 'fun']; ; // Change this line
+//     return sentence;
+//   }
+  
+//   console.log(spreadOut());
+
