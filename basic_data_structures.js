5edc7cd5fcdbab2ff4425f149f44d4f49e8221d0
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-16 01:08:45 +0300

    task 8

--- basic_data_structures.js
+++ basic_data_structures.js
@@ -63,3 +63,18 @@
 //   // Only change code above this line
 //   console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 
+//   8
+
+//   function copyMachine(arr, num) {
+//     let newArr = [];
+//     while (num >= 1) {
+//       // Only change code below this line
+//   newArr.push([...arr]);
+//       // Only change code above this line
+//       num--;
+//     }
+//     return newArr;
+//   }
+  
+//   console.log(copyMachine([true, false, true], 2));
+
