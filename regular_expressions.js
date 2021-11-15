565350861f6911cff7f86b82b39ca5b5943941ea
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-16 01:20:40 +0300

    task 9

--- regular_expressions.js
+++ regular_expressions.js
@@ -46,3 +46,9 @@
 // let vowelRegex = /[aeiou]/gi; // Change this line
 // let result = quoteSample.match(vowelRegex); // Change this line
 
+// 9
+
+// let quoteSample = "The quick brown fox jumps over the lazy dog.";
+// let alphabetRegex = /[a-z]/gi; // Change this line
+// let result = quoteSample.match(alphabetRegex); // Change this line
+
