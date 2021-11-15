d97d9b281d050055976b0642e7cee5aba37fc66a
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-16 01:07:02 +0300

    task 7

--- basic_data_structures.js
+++ basic_data_structures.js
@@ -52,4 +52,14 @@
   
 //   console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
 
-//   
+//   7
+
+//   function forecast(arr) {
+//     // Only change code below this line
+  
+//     return arr.slice(2, 4);
+//   }
+  
+//   // Only change code above this line
+//   console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
