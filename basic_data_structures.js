82506f984009f93f38c8ac9b93a689c0b6efb722
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-16 01:10:35 +0300

    task 10

--- basic_data_structures.js
+++ basic_data_structures.js
@@ -88,3 +88,16 @@
   
 //   console.log(spreadOut());
 
+//   10
+
+//   function quickCheck(arr, elem) {
+//     // Only change code below this line
+//   if (arr.indexOf(elem) >= 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//     // Only change code above this line
+//   }
+  
+//   console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
