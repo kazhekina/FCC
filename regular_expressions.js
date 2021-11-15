d1e68f5dcc0695224c5ec465728a5d9fdbd7fbd8
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-16 01:23:45 +0300

    task 14

--- regular_expressions.js
+++ regular_expressions.js
@@ -78,3 +78,9 @@
 
 // let result = chewieQuote.match(chewieRegex);
 
+// 14
+
+// let text = "<h1>Winter is coming</h1>";
+// let myRegex = /<.*?>/; // Change this line
+// let result = text.match(myRegex);
+
