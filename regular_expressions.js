fdc1858bc6f4506ed5bd0e408f13ab00d7c12047
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-16 01:15:33 +0300

    task 2

--- regular_expressions.js
+++ regular_expressions.js
@@ -4,3 +4,9 @@
 // let myRegex = /Hello/;
 // let result = myRegex.test(myString); // Change this line
 
+// 2
+
+// let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+// let waldoRegex = /Waldo/; // Change this line
+// let result = waldoRegex.test(waldoIsHiding);
+
