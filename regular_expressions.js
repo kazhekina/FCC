81b3ff8ed8af4e30b70a13ff44e2727dc906ffe2
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-16 01:18:24 +0300

    task 5

--- regular_expressions.js
+++ regular_expressions.js
@@ -22,3 +22,9 @@
 // let fccRegex = /freecodecamp/i; // Change this line
 // let result = fccRegex.test(myString);
 
+// 5
+
+// let extractStr = "Extract the word 'coding' from this string.";
+// let codingRegex = /coding/; // Change this line
+// let result = extractStr.match(codingRegex); // Change this line
+
