b09fe2178d1c87b1c773a98c3cc37616a7add43c
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-18 01:10:15 +0300

    task 76

--- basic_JS.js
+++ basic_JS.js
@@ -267,3 +267,31 @@
 // golfScore(5, 4);
 
 
+// 76
+
+
+// function caseInSwitch(val) {
+//   let answer = "";
+//   // Only change code below this line
+//   switch (val) {
+// case 1:
+//     return "alpha";
+//     break;
+// case 2:
+//     return "beta";
+//     break;
+// case 3:
+//     return "gamma";
+//     break;    
+// default:
+//     return "delta";
+//     break;
+//   }
+//   // Only change code above this line
+//   return answer;
+// }
+
+// caseInSwitch(1);
+
+
+
