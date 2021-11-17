fafc0e766c5b310245964a0cebc4209f1bdaa720
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-18 01:04:28 +0300

    task 66

--- basic_JS.js
+++ basic_JS.js
@@ -72,3 +72,21 @@
 //   testGreaterThan(10);
 
 
+//   66
+
+
+//   function testGreaterOrEqual(val) {
+//     if (val>=20) {  // Change this line
+//       return "20 or Over";
+//     }
+  
+//     if (val>=10) {  // Change this line
+//       return "10 or Over";
+//     }
+  
+//     return "Less than 10";
+//   }
+  
+//   testGreaterOrEqual(10);
+
+
