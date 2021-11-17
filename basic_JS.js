4408ebc44db9c81cf6f3ec5fd69c210eebc34b20
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-18 01:12:10 +0300

    task 80

--- basic_JS.js
+++ basic_JS.js
@@ -381,3 +381,15 @@
 // chainToSwitch(7);
 
 
+// 80
+
+
+// function isLess(a, b) {
+//   // Only change code below this line
+//   return a < b
+//   // Only change code above this line
+// }
+
+// isLess(10, 15);
+
+
