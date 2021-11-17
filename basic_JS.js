e438fc554b484005477b3b7169386ea776f3cf29
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-18 01:03:31 +0300

    task 64

--- basic_JS.js
+++ basic_JS.js
@@ -40,3 +40,17 @@
 //   testNotEqual(10);
 
 
+//   64
+
+
+//   // Setup
+// function testStrictNotEqual(val) {
+//     if (val !== 17) { // Change this line
+//       return "Not Equal";
+//     }
+//     return "Equal";
+//   }
+  
+//   testStrictNotEqual(10);
+
+
