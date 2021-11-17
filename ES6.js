9e23dfb9e2172dd0d64e72cc21fadcf7373c5a0c
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-17 23:36:57 +0300

    task 5

--- ES6.js
+++ ES6.js
@@ -55,3 +55,14 @@
 // const magic = () =>  new Date() 
 
 
+// 5
+
+
+// // change code below this line
+// const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+// // change code above this line
+// // test your code
+// console.log(myConcat([1, 2], [3, 4, 5]));
+
+
