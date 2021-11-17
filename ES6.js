e620b0dfbd8b0f98dc3f5d0c282db5f519820f86
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-17 23:42:08 +0300

    task 15

--- ES6.js
+++ ES6.js
@@ -175,3 +175,24 @@
 // // Only change code above this line
 
 
+// 15  
+
+
+// const result = {
+//   success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//   failure: ["no-var", "var-on-top", "linebreak"],
+//   skipped: ["no-extra-semi", "no-dup-keys"]
+// };
+// function makeList(arr) {
+//   // Only change code below this line
+//   const failureItems = [
+//   `<li class="text-warning">${result.failure[0]}</li>`,
+//   `<li class="text-warning">${result.failure[1]}</li>`,
+//   `<li class="text-warning">${result.failure[2]}</li>`
+//   ];
+//   // Only change code above this line
+//   return failureItems;
+// }
+// const failuresList = makeList(result.failure);
+
+
