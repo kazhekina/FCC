b38e19d4e204d6b0605e85810b140b5fb4e17bcb
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-18 01:02:21 +0300

    task 62

--- basic_JS.js
+++ basic_JS.js
@@ -12,3 +12,17 @@
 //   testStrict(10);
 
 
+//   62
+
+
+//   // Setup
+// function compareEquality(a, b) {
+//     if (a === b) { // Change this line
+//       return "Equal";
+//     }
+//     return "Not Equal";
+//   }
+  
+//   compareEquality(10, "10");
+
+
