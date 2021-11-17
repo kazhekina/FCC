2159332a99a27ca170f6cb5fe81d541cf81ebb01
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-17 23:39:13 +0300

    task 9

--- ES6.js
+++ ES6.js
@@ -93,3 +93,17 @@
 // console.log(arr2);
 
 
+// 9
+
+
+// const HIGH_TEMPERATURES = {
+//   yesterday: 75,
+//   today: 77,
+//   tomorrow: 80
+// };
+
+// // Only change code below this line
+// const { today, tomorrow } = HIGH_TEMPERATURES
+// // Only change code above this line
+
+
