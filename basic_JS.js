9955acf80e06fc4c8ab3c51e130a5957b6e795c6
Darya Kazhekina <daria.kazhekina@gmail.com>
2021-11-18 01:18:05 +0300

    task 85

--- basic_JS.js
+++ basic_JS.js
@@ -481,3 +481,17 @@
 // const shirtValue = testObj.shirt;    // Change this line
 
 
+// 85
+
+
+// // Setup
+// const testObj = {
+//   "an entree": "hamburger",
+//   "my side": "veggies",
+//   "the drink": "water"
+// };
+
+// // Only change code below this line
+// var entreeValue = testObj["an entree"];   // Change this line
+// var drinkValue = testObj["the drink"];    // Change this line
+
